686b2a3685beffc19e0ee23805a7c2c0c92190f9
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2016-03-23 16:49:04 +0100

    début du main et modif grille.h

--- Abonnenc_Bonhoure/Version1/include/grille.h
+++ Abonnenc_Bonhoure/Version1/include/grille.h
@@ -1,3 +1,5 @@
+#ifndef GRILLE_H
+#define GRILLE_H
 
 typedef char **Grille;
 
@@ -5,4 +7,8 @@ int afficher_jeu(Grille g, int taille);
  
 int affiche_etat_coules(Grille g, int taille);
 
+void allouer_grille(Grille g, int n);
+
 void remplir_grille(Grille g, int taille);
+
+#endif
